cbae4025d812a3aa7d80d0ccd11dc0ca1fdca045
rishibharadwajsai <rishi.lella123@gmail.com>
2024-03-12 22:20:07 +0530

    Update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require('path');
-const port = 8000;
+const port = process.env.PORT || 5000;
 require("./db/conn");
 
 const Register = require('./models/users');
