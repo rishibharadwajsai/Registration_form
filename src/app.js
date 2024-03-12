e849c12c6ea6af3937ca1d9339a1e86bf0309479
rishibharadwajsai <rishi.lella123@gmail.com>
2024-03-12 11:53:03 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const app = express();
 const path = require('path');
-const hbs = require("hbs");
 const port = 3000;
 require("./db/conn");
 
-
 const static_path = path.join(__dirname, '../public');
-const templates_path = path.join(__dirname, '../templates/views');
-const partials_path = path.join(__dirname, '../templates/partials');
-
 
 app.get("/", (req, res) => {
     res.sendFile('public/index.html');
