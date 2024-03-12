aeff6098769e811ff9d20857cd01c4312fd904ea
rishibharadwajsai <rishi.lella123@gmail.com>
2024-03-12 20:45:19 +0530

    Update

--- src/app.js
+++ src/app.js
@@ -1,25 +1,33 @@
 const express = require("express");
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = 8000;
 require("./db/conn");
+
+const Register = require('./models/users');
 
 const static_path = path.join(__dirname, '../public');
 
 app.use(express.static(static_path));
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
 
 app.get("/", (req, res) => {
     const html_path = path.join(static_path, 'index.html');
     res.sendFile(html_path);
 });
 
-app.post("/" ,async(req,res) => {
+app.post("/", async (req, res) => {
     try {
-        
+        if (req.body.submitBtn === 'Submit') {
+            console.log(req.body.email);
+
+            res.redirect(html_path);
+        }
     } catch (error) {
-        
+        res.status(400).send(error);
     }
-})
+});
 
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`);
--- src/models/users.js
+++ src/models/users.js
@@ -1,23 +1,23 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    First_name : {
-        type : String,
-        required : true
+    First_name: {
+        type: String,
+        required: true
     },
-    email : {
-        type : String,
-        required : true,
+    email: {
+        type: String,
+        required: true,
         unique: true
     },
-    mobile : {
-        type : Number,
-        required : true,
-        unique : true
+    mobile: {
+        type: Number,
+        required: true,
+        unique: true
     },
-    password : {
-        type : String,
-        required : true
+    password: {
+        type: String,
+        required: true
     },
 });
 
